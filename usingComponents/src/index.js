96fb8491efb54358c702272ebac715b7e97cb358
cpustejovsky <charles.pustejovsky@gmail.com>
2019-12-31 07:43:16 -0500

    finished up the usingComponenents mini-project; starting on a simple weather app;

--- usingComponents/src/index.js
+++ usingComponents/src/index.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import CommentDetails from './components/CommentDetail';
+import CommentDetail from './components/CommentDetail';
+import ApprovalCard from './components/ApprovalCard';
 
 const App = () => {
 
   return (
     <div className="ui container comments">
-      <CommentDetails
-        name={faker.name.firstName()}
-        timeAgo={faker.date.weekday()}
-        avatarImg={faker.image.avatar()}
-        content={faker.hacker.phrase()}
-      />
-      <CommentDetails
-        name={faker.name.firstName()}
-        timeAgo={faker.date.weekday()}
-        avatarImg={faker.image.avatar()}
-        content={faker.hacker.phrase()}
-      />
-      <CommentDetails
-        name={faker.name.firstName()}
-        timeAgo={faker.date.weekday()}
-        avatarImg={faker.image.avatar()}
-        content={faker.hacker.phrase()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          name={faker.name.firstName()}
+          timeAgo={faker.date.weekday()}
+          avatarImg={faker.image.avatar()}
+          content={faker.hacker.phrase()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          name={faker.name.firstName()}
+          timeAgo={faker.date.weekday()}
+          avatarImg={faker.image.avatar()}
+          content={faker.hacker.phrase()}
+        />
+      </ApprovalCard>      <ApprovalCard>
+        <CommentDetail
+          name={faker.name.firstName()}
+          timeAgo={faker.date.weekday()}
+          avatarImg={faker.image.avatar()}
+          content={faker.hacker.phrase()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
