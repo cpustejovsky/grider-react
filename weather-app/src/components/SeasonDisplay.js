6277ab2b7f1d3cfcba45894db8a8299ff1cb3467
cpustejovsky <charles.pustejovsky@gmail.com>
2019-12-31 15:22:00 -0500

    one more commit before heading home

--- weather-app/src/components/SeasonDisplay.js
+++ weather-app/src/components/SeasonDisplay.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 export default class SeasonDisplay extends Component {
-
+  //Good place for one-time setup
   constructor(props) {
     super(props);
 
@@ -9,19 +9,23 @@ export default class SeasonDisplay extends Component {
       latitude: null,
       errorMsg: ""
     }
-
+  }
+  //Good place to do data-loading
+  componentDidMount() {
+    console.log("COMPONENENT MOUNTED");
     const success = (pos) => this.setState({ latitude: pos.coords.latitude })
     const error = (err) => this.setState({ errorMsg: err.message })
     window.navigator.geolocation.getCurrentPosition(success, error);
   }
-  componentDidMount() {
-    console.log("COMPONENENT MOUNTED");
-  }
-
+  //Good place to do more data-loading when state/props change
   componentDidUpdate() {
     console.log("COMPONENT UPDATED")
   }
-
+  //Good place to do clean up, especially for non-React stuff
+  componentWillUnmount() {
+    console.log("COMPONENENT WILL UNMOUNT")
+  }
+  //Avoid doing anything besides returning JSX
   render() {
     if (this.state.errorMsg && !this.state.latitude) {
       return (
@@ -37,7 +41,7 @@ export default class SeasonDisplay extends Component {
     if (!this.state.errorMsg && this.state.latitude) {
       return (
         <div className="ui container">
-          <div className="ui message">
+          <div className="ui success message">
             <div className="header">
               Latitude: {this.state.latitude}
             </div>
