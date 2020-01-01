8169d21fcff9afa77293fb8f56f11a8ef7493a22
cpustejovsky <charles.pustejovsky@gmail.com>
2019-12-31 21:08:14 -0500

    got the core functioanlity of the little app working;

--- weather-app/src/components/SeasonDisplay.js
+++ weather-app/src/components/SeasonDisplay.js
@@ -1,58 +1,36 @@
-import React, { Component } from 'react'
+import '../style/SeasonDisplay.css'
+import React from "react";
 
-export default class SeasonDisplay extends Component {
-  //Good place for one-time setup
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      latitude: null,
-      errorMsg: ""
-    }
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, it's chilly!",
+    iconName: "snowflake"
   }
-  //Good place to do data-loading
-  componentDidMount() {
-    console.log("COMPONENENT MOUNTED");
-    const success = (pos) => this.setState({ latitude: pos.coords.latitude })
-    const error = (err) => this.setState({ errorMsg: err.message })
-    window.navigator.geolocation.getCurrentPosition(success, error);
-  }
-  //Good place to do more data-loading when state/props change
-  componentDidUpdate() {
-    console.log("COMPONENT UPDATED")
-  }
-  //Good place to do clean up, especially for non-React stuff
-  componentWillUnmount() {
-    console.log("COMPONENENT WILL UNMOUNT")
-  }
-  //Avoid doing anything besides returning JSX
-  render() {
-    if (this.state.errorMsg && !this.state.latitude) {
-      return (
-        <div className="ui container">
-          <div className="ui negative message">
-            <div className="header">
-              {this.state.errorMsg}
-            </div>
-          </div>
-        </div>
-      )
-    }
-    if (!this.state.errorMsg && this.state.latitude) {
-      return (
-        <div className="ui container">
-          <div className="ui success message">
-            <div className="header">
-              Latitude: {this.state.latitude}
-            </div>
-          </div>
-        </div>
-      )
-    }
-    if (!this.state.errorMsg && !this.state.latitude) {
-      return <div></div>
-    }
-
-  }
-
 }
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  }
+  else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.latitude, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+  return (
+      <div className={`season-display ${season}`}>
+        <i className={`${iconName} icon-left icon massive`}></i>
+        <h1>{text}</h1>
+        <i className={`${iconName} icon-right icon massive`}></i>
+      </div>
+  );
+};
+
+export default SeasonDisplay;
--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,13 +1,53 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './components/SeasonDisplay'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./components/SeasonDisplay";
 
 export default class App extends Component {
+  //Good place for one-time setup
+  state = {
+    latitude: null,
+    errorMsg: ""
+  };
+
+  //Good place to do data-loading
+  componentDidMount() {
+    const success = pos => this.setState({ latitude: pos.coords.latitude });
+    const error = err => this.setState({ errorMsg: err.message });
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    window.navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+  //Good place to do more data-loading when state/props change
+  componentDidUpdate() {}
+  //Good place to do clean up, especially for non-React stuff
+  componentWillUnmount() {}
+  //Avoid doing anything besides returning JSX
   render() {
-    return (
-      <SeasonDisplay></SeasonDisplay>
-    )
+    if (this.state.errorMsg && !this.state.latitude) {
+      return (
+        <div className="ui container">
+          <div className="ui negative message">
+            <div className="header">{this.state.errorMsg}</div>
+          </div>
+        </div>
+      );
+    }
+    if (!this.state.errorMsg && this.state.latitude) {
+      return <SeasonDisplay latitude={this.state.latitude} />;
+    }
+    if (!this.state.errorMsg && !this.state.latitude) {
+      return (
+        <div className="ui container">
+          <div className="ui message">
+            <div className="header">Season App</div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
