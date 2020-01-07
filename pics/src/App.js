cb6cdaab4aac6ced51ea92aecfcf0fb89c53d339
cpustejovsky <charles.pustejovsky@gmail.com>
2020-01-07 17:15:00 -0500

    working on async data request;

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
 import ImageList from './components/ImageList';
 
 export default class App extends Component {
+  state = { images: [] };
 
-  onSearchSubmit(term) {
-    console.log(term)
+  async onSearchSubmit(term) {
+    const rootUrl = "https://api.unsplash.com/";
+    const response = await axios.get(`${rootUrl}/search/photos`, {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+      }
+    })
+    this.setState({ this.state.images: response.data.results });
+    console.log(this.state.images)
+    console.log(process.env.REACT_APP_UNSPLASH_ACCESS_KEY)
+    console.log(process.env.REACT_APP_UNSPLASH_SECRET_KEY)
   }
 
   render() {
