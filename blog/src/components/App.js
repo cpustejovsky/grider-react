8ab8bc6ee9addd0009ecb3cb229fb57cd6b50f55
cpustejovsky <charles.pustejovsky@gmail.com>
2020-01-20 21:01:31 -0500

    setting up project to better understand redux-thunk; Very interesting. Still don't fully understand it. Perhaps when it's connected to reducers, I'll have a better idea."

---
+++ blog/src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PostList from "./PostList";
+export default function App() {
+  return (
+    <div className="ui container">
+      <PostList></PostList>
+    </div>
+  );
+}
