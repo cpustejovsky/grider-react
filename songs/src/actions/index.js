8e8cd51bf40cfe9eba1486f32582a86f84101014
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-15 15:00:42 -0400

    changes from exercises

--- songs/src/actions/index.js
+++ songs/src/actions/index.js
@@ -1,5 +1,6 @@
 //Action creator
 export const selectSong = (song) => {
+    //return an action
     return {
         type: "SONG_SELECTED",
         payload: song
--- songs/src/index.js
+++ songs/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-// import { createStore } from "redux";
+import { createStore } from "redux";
 
 import App from "./components/App";
-// import { selectSong } from "../actions";
+import { selectSong } from "./actions";
 import store from "./reducers";
 
 ReactDOM.render(
