264e65e6e12cbb3d05b3161385298b0916d09c2f
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-18 19:51:35 -0400

    component boilerplate and routing setup

--- twitch-clone/client/src/components/App.jsx
+++ twitch-clone/client/src/components/App.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-//   return <div>PageOne <button><a href="/pagetwo">Page Two</a></button></div>;
-  return <div>PageOne <button><Link to="/pagetwo">Page Two</Link></button></div>;
-};
-
-const PageTwo = () => {
-//   return <div>PageTwo <button><a href="/">Page One</a></button></div>;
-  return <div>PageTwo <button><Link to="/">Page One</Link></button></div>;
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" exact component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
