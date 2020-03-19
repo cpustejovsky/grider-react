5ac9f8a45124f81431deb674a2e4e6f31d4189b4
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-19 06:19:00 -0400

    setting up links in header and moving header within BrowserRouter

--- twitch-clone/client/src/components/Header.jsx
+++ twitch-clone/client/src/components/Header.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <div>
-            Header
-            <hr/>
-        </div>
-    )
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamy
+      </Link>
+      <div className="right menu">
+        {" "}
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+      </div>
+    </div>
+  );
 }
