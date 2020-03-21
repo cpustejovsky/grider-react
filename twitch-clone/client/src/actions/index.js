4bf9e3f8e20648735850a66a9d57a2a36874c8a2
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-21 16:43:10 -0400

    setting up action creators for restful routing

--- twitch-clone/client/src/actions/index.js
+++ twitch-clone/client/src/actions/index.js
@@ -1,4 +1,12 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
 import streams from "../apis/streams";
 
 export const signIn = userId => {
@@ -15,9 +23,41 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-    const response = await streams.post('/streams', formValues)
-    return {
-        type: CREATE_STREAM,
-        payload: response
-    }
+  const response = await streams.post("/streams", formValues);
+  dispatch({
+    type: CREATE_STREAM,
+    payload: response
+  });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+  dispatch({
+    type: FETCH_STREAMS,
+    payload: response.data
+  });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({
+    type: FETCH_STREAM,
+    payload: response.data
+  });
+};
+
+export const editStream = (formValues, id) => async dispatch => {
+  const response = await streams.put(`/streams/:${id}`, formValues);
+  dispatch({
+    type: EDIT_STREAM,
+    payload: response.data
+  });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/:${id}`);
+  dispatch({
+    type: DELETE_STREAM,
+    payload: id
+  });
 };
