2f797d2a412ce68a96b81357b515c592e3532603
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-27 12:57:14 -0400

    Hooked on Hooks!

--- hooks-app/src/components/App.jsx
+++ hooks-app/src/components/App.jsx
@@ -1,14 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ResourceList from "./ResourceList";
+import UserList from "./UserList";
 const App = () => {
   const [resource, setResource] = useState("posts");
   return (
-    <div>
+    <div className="container">
       <div>
         <button onClick={() => setResource("posts")}>Posts</button>
         <button onClick={() => setResource("todos")}>Todos</button>
       </div>
-      <ResourceList resource={resource} />
+      <hr/>
+      <div className="columns">
+        <ResourceList resource={resource} className="column" />
+        <UserList resource={"users"} className="column" />
+      </div>
     </div>
   );
 };
--- hooks-app/src/components/UserList.jsx
+++ hooks-app/src/components/UserList.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import useResources from "./useResources";
 
-const ResourceList = ({ resource }) => {
+const UserList = ({ resource }) => {
   const resources = useResources(resource);
 
   const renderList = resources => {
     return resources.map(resource => {
-      return <li key={resource.id}>{resource.title}</li>;
+      return <li key={resource.id}>{resource.name}</li>;
     });
   };
   console.log(resources[0]);
@@ -20,4 +20,4 @@ const ResourceList = ({ resource }) => {
   );
 };
 
-export default ResourceList;
+export default UserList;
